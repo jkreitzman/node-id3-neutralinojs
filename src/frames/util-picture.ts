77799b27ece838e96b7758b0c2d65db9449d6b71
jkreitzman <jkreitzman@users.noreply.github.com>
2023-07-09 12:48:06 -0700

    Fix missed fs ref

--- src/frames/util-picture.ts
+++ src/frames/util-picture.ts
@@ -1,36 +1,42 @@
-import * as fs from "fs"
 import { isBuffer } from "../util"
 
 function getPictureMimeTypeFromBuffer(pictureBuffer: Buffer) {
-    if (
-        pictureBuffer.length > 3 &&
-        pictureBuffer.compare(Buffer.from([0xff, 0xd8, 0xff]), 0, 3, 0, 3) === 0
-    ) {
-        return "image/jpeg"
-    }
-    if (
-        pictureBuffer.length > 8 &&
-        pictureBuffer.compare(Buffer.from([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]), 0, 8, 0, 8) === 0
-    ) {
-        return "image/png"
-    }
-    return ""
+  if (
+    pictureBuffer.length > 3 &&
+    pictureBuffer.compare(Buffer.from([0xff, 0xd8, 0xff]), 0, 3, 0, 3) === 0
+  ) {
+    return "image/jpeg"
+  }
+  if (
+    pictureBuffer.length > 8 &&
+    pictureBuffer.compare(
+      Buffer.from([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]),
+      0,
+      8,
+      0,
+      8
+    ) === 0
+  ) {
+    return "image/png"
+  }
+  return ""
 }
 
 export function retrievePictureAndMimeType({
-    filenameOrBuffer,
-    mimeType
+  filenameOrBuffer,
+  mimeType,
 }: {
-    filenameOrBuffer?: Buffer | string
-    mimeType?: string
+  filenameOrBuffer?: Buffer | string
+  mimeType?: string
 }) {
-    if (!filenameOrBuffer) {
-        throw new TypeError("Missing image buffer or filename")
-    }
-    const pictureBuffer = isBuffer(filenameOrBuffer) ?
-        filenameOrBuffer : fs.readFileSync(filenameOrBuffer)
-    return {
-        pictureBuffer,
-        mimeType: mimeType ?? getPictureMimeTypeFromBuffer(pictureBuffer)
-    }
+  if (!filenameOrBuffer) {
+    throw new TypeError("Missing image buffer or filename")
+  }
+  if (!isBuffer(filenameOrBuffer)) {
+    throw new TypeError("Cannot use a filename with this function")
+  }
+  return {
+    pictureBuffer: filenameOrBuffer,
+    mimeType: mimeType ?? getPictureMimeTypeFromBuffer(filenameOrBuffer),
+  }
 }
